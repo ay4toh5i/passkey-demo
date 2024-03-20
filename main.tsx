13f291fa6a5f9864e8e3b4375cb84ff8a00f04f8
Ayato <ay4toh5i@gmail.com>
2024-03-20 16:08:48 +0900

    Add jsx middleware

--- main.tsx
+++ main.tsx
@@ -1,9 +1,11 @@
+/** @jsx jsx */
 import { Hono } from 'https://deno.land/x/hono@v4.1.2/mod.ts'
+import { jsx } from 'https://deno.land/x/hono@v4.1.2/middleware.ts'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.html(<h1>Hello Deno!</h1>)
 })
 
 Deno.serve(app.fetch)
